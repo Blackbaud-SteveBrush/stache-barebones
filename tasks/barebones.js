e931146e95b87001d4b7c3a8aede84cc77aa4316
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2015-11-22 17:06:44 -0500

    Adding YAML support

--- tasks/barebones.js
+++ tasks/barebones.js
@@ -1,21 +1,43 @@
 "use strict";
-
 module.exports = function (grunt) {
 
+    /**
+     * Merges two objects into one.
+     * https://github.com/yeikos/js.merge
+     */
+    var merge = require('merge');
+
+    /**
+     * JIT (Just In Time) plugin loader for Grunt.
+     * https://github.com/shootaroo/jit-grunt
+     */
+    var jit = require('jit-grunt');
+
+    // Plugin defaults.
     var defaults = {
+        stache: {
+            dir: 'node_modules/stache-barebones/',
+            config: {},
+            configFile: 'stache.yml'
+        },
+        clean: {
+            build: {
+                src: ['build/']
+            }
+        },
         assemble: {
             options: {
-                layoutdir: 'node_modules/stache-barebones/src/layouts/',
-                layoutext: '.hbs',
-                layout: 'layout-default',
+                layout: 'default',
+                layoutdir: '<%= stache.dir %>src/layouts/',
+                layoutext: '.hbs'
             },
             default: {
                 options: {},
                 files: [
                     {
                         expand: true,
-                        cwd: 'content/',
-                        dest: 'serve/',
+                        cwd: 'content/pages/',
+                        dest: 'build/',
                         src: [
                             '**/*.md',
                             '**/*.hbs',
@@ -30,13 +52,15 @@ module.exports = function (grunt) {
                 style: 'compressed'
             },
             dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'node_modules/stache-barebones/src/styles',
-                    src: ['*.scss'],
-                    dest: 'serve/dist/styles/',
-                    ext: '.css'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'content/styles/',
+                        src: ['*.scss'],
+                        dest: 'build/assets/styles/',
+                        ext: '.css'
+                    }
+                ]
             }
         },
         connect: {
@@ -44,7 +68,7 @@ module.exports = function (grunt) {
                 options: {
                     port: 4000,
                     livereload: 4100,
-                    base: 'serve'
+                    base: 'build'
                 }
             }
         },
@@ -54,7 +78,7 @@ module.exports = function (grunt) {
             },
             stache: {
                 files: [
-                    'node_modules/stache-barebones/src/**/*.*',
+                    '<%= stache.dir %>src/**/*.*',
                 ],
                 tasks: [
                     'assemble'
@@ -63,17 +87,27 @@ module.exports = function (grunt) {
         }
     };
 
+    /**
+     * Retrieve and compile the appropriate YAML files and load it into
+     * the plugin defaults.
+     */
+    var stacheConfig = grunt.file.readYAML(defaults.stache.dir + defaults.stache.configFile);
+    var localConfig = grunt.file.readYAML(defaults.stache.configFile);
+    defaults.stache.config = merge(stacheConfig, localConfig);
     grunt.config.merge(defaults);
 
+    // Load NPM tasks dynamically.
+    jit(grunt)({
+        pluginsRoot: defaults.stache.dir + 'node_modules/'
+    });
+
+    // Grunt tasks.
     grunt.registerTask('serve', [
+        'clean',
         'assemble',
         'sass',
         'connect',
         'watch'
     ]);
-
-    require('jit-grunt')(grunt)({
-        pluginsRoot: 'node_modules/stache-barebones/node_modules/'
-    });
 
 };
